03fec78ef58e4eee01ed582d8f7bcf650523c8e5
AKASH CHONDRO ROY <akt311470@gmail.com>
2026-09-03 16:31:28 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-base: '/AKASH-STORE/',
+base: '/akashstore/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
